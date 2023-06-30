855e17a2dd78d74d002ff333e28622979a4f1c94
rnldbss <ronaldbessam@gmail.com>
2023-06-29 22:17:25 -0300

    contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { contact } from "../styles/contact.module.css";
+import { StaticImage } from "gatsby-plugin-image";
+import SendIcon from "./svg/SendIcon";
+import {
+  contact,
+  contactEmail,
+  contactEmailSend,
+  contactImg,
+  sendIcon,
+} from "../styles/contact.module.css";
 
 function Contact() {
   return (
     <section className={contact}>
-      <h2>Ready?</h2>
-      <h3>hello@bessa.dev</h3>
+      <div className={contactEmail}>
+        <h2>Ready?</h2>
+        <div className={contactEmailSend}>
+          <SendIcon sendIcon={sendIcon} />
+          <h3>hello@bessa.dev</h3>
+        </div>
+      </div>
+      <div className={contactImg}>
+        <StaticImage src="../images/me.jpg" />
+      </div>
     </section>
   );
 }
